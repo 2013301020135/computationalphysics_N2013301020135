f1c7652b5e7433586f2702fd3af7e6aac3391262
2013301020135 <gary.moriarty@qq.com>
2016-05-16 18:00:51 +0800

    Update Ex07-L1.py

--- Chapter-2/Exercise-7/Ex07-L1.py
+++ Chapter-2/Exercise-7/Ex07-L1.py
@@ -6,25 +6,22 @@ dt=0.01
 
 print 'Input baseball type (0 for rough, 1 for normal, 2 for smooth) ->',
 balltype = float(raw_input())
-basetype = 0
-
+if (balltype != 0)and(balltype != 1)and(balltype != 2):
+    print 'Baseball type error!'
+    print 'Calculate as normal baseball!'
 
 def B2m(v):
     'Calculate air resistance factor'
     if (balltype == 0):
         B2m=(0.0013+0.0084/(1+math.exp((v-21)/2.5)))*(4-(3-math.exp((v-39)/2))/(1+math.exp((v-39)/2)))
-        basetype = 1
     if (balltype == 1):
         B2m=0.0039+0.0058/(1+math.exp((v-35)/5))
-        basetype = 1
     if (balltype == 2):
         B2m=0.0013+0.0084/(1+math.exp((v-65)/4))
-        basetype = 1
-    if (basetype == 0):
-        print 'Baseball type error!'
-        print 'Calculate as normal baseball!'
+    if (balltype != 0)and(balltype != 1)and(balltype != 2):
         B2m=0.0039+0.0058/(1+math.exp((v-35)/5))
     return B2m
+
 
 print 'Input angular velocity ->',
 omega = float(raw_input())
